33c091a8a3451e739baadb87167857076cc19b15
Eric F. <effr22@gmail.com>
2014-02-16 12:43:27 -0500

    Changes in Main Screen Layout
    Added artist name for each list item. Also, resized the option buttons.

--- Bitbox/src/com/teambitbox/bitbox/MainScreenActivity.java
+++ Bitbox/src/com/teambitbox/bitbox/MainScreenActivity.java
@@ -14,7 +14,9 @@ import android.widget.TextView;
 public class MainScreenActivity extends Activity {
 
 	private ListView filelist;
-    String[] filenames = new String[] { "Android", "iPhone", "WindowsMobile",
+	
+	// string for file names
+    private String[] filenames = new String[] { "Android", "iPhone", "WindowsMobile",
 	            "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
 	            "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
 	            "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
@@ -25,11 +27,12 @@ public class MainScreenActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
         
-        filelist = (ListView) findViewById(R.id.listView);
+        filelist = (ListView) findViewById(R.id.listView); // initializes ListView
        
         
-        filelist.setAdapter(new IconicAdapter());
+        filelist.setAdapter(new SongListAdapter()); // sets adapter for ListView
 		
+        
 		filelist.setOnItemClickListener(new AdapterView.OnItemClickListener() 
 		{
 	      @Override
@@ -42,8 +45,9 @@ public class MainScreenActivity extends Activity {
 
     }
     
-    class IconicAdapter extends ArrayAdapter<String> {
-		    IconicAdapter() {
+    // extends ArrayAdapter Class; this adapter is used to create the Text Views for each list item
+    class SongListAdapter extends ArrayAdapter<String> {
+		    SongListAdapter() {
 		      super(MainScreenActivity.this, R.layout.rowlayout, filenames);
 		    }
 		    
@@ -51,16 +55,17 @@ public class MainScreenActivity extends Activity {
 		                        ViewGroup parent) {
 		      View row=convertView;
 		      
-		      if (row==null) {                          
+		      if (row == null) {                          
 		        LayoutInflater inflater=getLayoutInflater();
 		        
 		        row=inflater.inflate(R.layout.rowlayout, parent, false);
 		      }
 		      
-		      TextView label=(TextView)row.findViewById(R.id.label);
-		      label.setText(filenames[position]);
-
+		      // initializes textView
+		      TextView label = (TextView)row.findViewById(R.id.fileNameLabel);
 		      
+		      // uses filenames array to set the text for each list item depending on position
+		      label.setText(filenames[position]);
 		      
 		      return(row);
 
